833cfc0d15c19fc50e6ab4db71a3a766f74fdd6e
suhail79 <iamsuhail79@gmail.com>
2026-07-18 16:47:13 +0530

    feat(terraform): create VPC and public subnet

--- terraform/03-vpc.tf
+++ terraform/03-vpc.tf
@@ -10,17 +10,3 @@ resource "aws_vpc" "main" {
   }
 
 }
-resource "aws_subnet" "public" {
-
-  vpc_id = aws_vpc.main.id
-
-  cidr_block = var.public_subnet_cidr
-
-  availability_zone = "ap-south-1a"
-
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet"
-  }
-}
